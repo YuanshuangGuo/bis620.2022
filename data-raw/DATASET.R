13b8b7c4f02fdad569d84cadfde49f4e8a9f963e
Stella GUO <yuanshuang.guo@yale.edu>
2023-12-03 23:01:16 -0500

    upload data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,3 +2,10 @@
 
 accel = readRDS("accel.rds")
 usethis::use_data(accel, overwrite = TRUE)
+
+condi = load("condi.RData")
+usethis::use_data(condi, overwrite = TRUE)
+
+studies = load("studies.RData")
+usethis::use_data(studies, overwrite = TRUE)
+
